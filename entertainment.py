98a00edd54c1e6cf80bd834db07f45dfbdc5895f
willsongs <90972685+willsongs@users.noreply.github.com>
2022-08-07 18:55:33 +0530

    Add files via upload

---
+++ entertainment.py
@@ -0,0 +1,48 @@
+import telebot
+import requests
+import json
+
+from telebot import custom_filters
+from telebot import types
+import mysql.connector
+
+API_TOKEN = '5505287179:AAFu9iQ7jTeY8JM_KTN7hPe6oUawkYI195A'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    try:
+        markup = types.ReplyKeyboardMarkup(row_width=2)
+        bot.reply_to(message, 'okey! now enter any name of movie or webseries you want to watch today', reply_markup=markup)
+    except Exception as e:
+        bot.reply_to(message, 'oooops')
+
+@bot.message_handler(regexp=r'\b[ a-zA-Z.]+\b')
+def input(message):
+    try:
+        term = message.text
+        # print(term)
+        url = requests.get(f"https://doodapi.com/api/search/videos?key=13527p8pcv54of4yjeryk&search_term={term}")
+        data = url.text
+        parse_json = json.loads(data)
+
+        n = len(parse_json['result'])
+        if n==0:
+            bot.reply_to(message,'the movie is not in the database right now. Will be added to the database soon')
+            bot.send_message(message.chat.id, 'Please try again after some time')
+
+        else:
+            for i in range(n):
+                # print(parse_json['result'][i])
+                code = parse_json['result'][i]['file_code']
+                img = parse_json['result'][i]['splash_img']
+                # print(f"https://dood.wf/d/{code}")
+                # print(img)
+                bot.send_photo(message.chat.id, img, f"https://dood.wf/d/{code}")
+
+    except Exception as e:
+        bot.reply_to(message, 'oooops')
+
+
+bot.polling()
