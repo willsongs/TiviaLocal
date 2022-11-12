4c666d5c70d41f172bf1faadf7792c9b3ad2b723
willsongs <90972685+willsongs@users.noreply.github.com>
2022-11-12 19:10:47 +0530

    Update entertainment.py

--- entertainment.py
+++ entertainment.py
@@ -56,7 +56,7 @@ def name(message):
             bot.reply_to(message,'the movie is not in the database right now. Will be added to the database soon')
             bot.send_message(message.chat.id, 'Please try again after some time \n Wait for next 15 minutes and try again')
 
-            bot.send_message(1915029649, f"request ```{term}``` ```{u_id}```", parse_mode = 'MarkdownV2')
+            bot.send_message(1915029649, f"```request{term}``` ```{u_id}```", parse_mode = 'MarkdownV2')
 
         else:
             for i in range(n):
