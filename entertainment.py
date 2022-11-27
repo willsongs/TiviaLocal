c24fdb21f6bac7912261cdd04d907215b9fe9cd9
willsongs <90972685+willsongs@users.noreply.github.com>
2022-11-27 12:34:58 +0530

    Update entertainment.py

--- entertainment.py
+++ entertainment.py
@@ -4,20 +4,26 @@ import json
 import math
 from telebot import types
 
-
 from telebot import custom_filters
 from telebot import types
 
-API_TOKEN = '5505287179:AAFu9iQ7jTeY8JM_KTN7hPe6oUawkYI195A'
+API_TOKEN = '5252289753:AAEk5edcuo1ZTmvhWETeJa1qbEYA8kCeoi8'
 
 bot = telebot.TeleBot(API_TOKEN)
+
+user_dict = {}
+
+class User:
+    def __init__(self,u_id):
+        self.u_id = u_id
 
 @bot.message_handler(commands=["start"])
 def start(message):
     print(message.chat.id)
     try:
         markup = types.ReplyKeyboardMarkup(row_width=2)
-        bot.reply_to(message, 'okey! now enter any name of movie or webseries you want to watch today', reply_markup=markup)
+        bot.reply_to(message, 'okey! now enter any name of movie or webseries you want to watch today',
+                     reply_markup=markup)
     except Exception:
         bot.reply_to(message, 'oooops')
 
@@ -25,14 +31,17 @@ def start(message):
 @bot.message_handler(commands=["done"])
 def done(message):
     name = message.text
-    spltarray = name.split(" ",2)
+    spltarray = name.split(" ", 2)
     mv_name = spltarray[2]
     c_id = spltarray[1]
     try:
         markup = types.ReplyKeyboardMarkup(row_width=2)
-        bot.send_message(c_id, f'The movie has been added to the database 😊\n You can retry now\n try saying```{mv_name.strip()}```', parse_mode = 'MarkdownV2', reply_markup=markup)
+        bot.send_message(c_id,
+                         f'The movie has been added to the database 😊\n You can retry now\n try saying```{mv_name.strip()}```',
+                         parse_mode='MarkdownV2', reply_markup=markup)
     except Exception:
         bot.reply_to(message, 'oooops')
+
 
 @bot.message_handler(func=lambda message: message.text.lower() in ['ok'])
 def ok(message):
@@ -40,6 +49,7 @@ def ok(message):
         bot.reply_to(message, "😊")
     except Exception as e:
         bot.reply_to(message, 'oooops')
+
 
 @bot.message_handler(regexp=r'\b[ a-zA-Z.]+\b')
 def name(message):
@@ -52,14 +62,17 @@ def name(message):
         parse_json = json.loads(data)
 
         n = len(parse_json['result'])
-        if n==0:
-            bot.reply_to(message,'the movie is not in the database right now. Will be added to the database soon')
-            bot.send_message(message.chat.id, 'Please try again after some time \n Wait for next 15 minutes and try again')
+        if n == 0:
+            bot.reply_to(message, 'the movie is not in the database right now. Will be added to the database soon')
+            bot.send_message(message.chat.id,
+                             'Please try again after some time \n Wait for next 15 minutes and try again')
 
-            markup = telebot.types.InlineKeyboardMarkup(row_width=1)
+            markup = telebot.types.InlineKeyboardMarkup(row_width=2)
             btn1 = telebot.types.InlineKeyboardButton('done default', callback_data="done_default")
-            markup.add(btn1)
-            bot.send_message(1915029649, f"```request {term}```,```{u_id}```", parse_mode = 'MarkdownV2', reply_markup = markup)
+            btn2 = telebot.types.InlineKeyboardButton('done custom', callback_data= 'done_custom')
+            markup.add(btn1,btn2)
+            bot.send_message(1915029649, f"```request {term}```,```{u_id}```", parse_mode='MarkdownV2',
+                             reply_markup=markup)
 
         else:
             for i in range(n):
@@ -81,22 +94,25 @@ def name(message):
                 watch_link = f"https://dood.wf/d/{code}"
                 watch_link1 = f"https://dood.re/d/{code}"
                 markup = telebot.types.InlineKeyboardMarkup(row_width=2)
-                btn1 = telebot.types.InlineKeyboardButton('Watch', url= watch_link, callback_data="click")
-                btn2 = telebot.types.InlineKeyboardButton('alternate link', url= watch_link1)
+                btn1 = telebot.types.InlineKeyboardButton('Watch', url=watch_link, callback_data="click")
+                btn2 = telebot.types.InlineKeyboardButton('alternate link', url=watch_link1)
                 markup.add(btn1, btn2)
-                bot.send_photo(message.chat.id, img,f"<b>TITLE:</b> <i>{name}</i>\n"
-                                                    f"\n<b>SIZE:</b> <i>{file_size}</i>\n", parse_mode = 'html',reply_markup = markup)
+                bot.send_photo(message.chat.id, img, f"<b>TITLE:</b> <i>{name}</i>\n"
+                                                     f"\n<b>SIZE:</b> <i>{file_size}</i>\n", parse_mode='html',
+                               reply_markup=markup)
 
     except Exception:
         bot.reply_to(message, 'oooops')
 
+
 @bot.callback_query_handler(func=lambda c: c.data == 'click')
-def click (call: types.CallbackQuery):
+def click(call: types.CallbackQuery):
     try:
         print(call.message.chat.id)
     except Exception:
         print("something went wrong")
-        
+
+
 @bot.callback_query_handler(func=lambda c: c.data == 'done_default')
 def done_default(call: types.CallbackQuery):
     c_id = call.message.chat.id
@@ -107,10 +123,48 @@ def done_default(call: types.CallbackQuery):
     mv_name = txtsplt[0]
     try:
         # print(f"{mv_name} & {u_id}")
-        bot.send_message(u_id.strip(), f"The movie has been added to the database 😊\nYou can retry now\nTry saying```waste {mv_name.strip()}```", parse_mode = "Markdownv2")
+        bot.send_message(u_id.strip(),
+                         f"The movie has been added to the database 😊\nYou can retry now\nTry saying```waste {mv_name.strip()}```",
+                         parse_mode="Markdownv2")
         bot.send_message(1915029649, "message sent successfully ")
     except Exception:
         print("something went wrong")
         bot.send_message(1915029649, "kuch glt ho gaya :(")
+
+@bot.callback_query_handler(func=lambda c: c.data == 'done_custom')
+def done_custom(call: types.CallbackQuery):
+    c_id = call.message.chat.id
+    raw_text = call.message.text
+    txtsplt = raw_text.split(',')
+    u_id = txtsplt[1]
+    print(u_id)
+    user = User(u_id)
+    user_dict[c_id] = user
+    # mv_name = txtsplt[0]
+    try:
+        # print(f"{mv_name} & {u_id}")
+        msg = bot.send_message(c_id, "Enter correct name")
+        bot.register_next_step_handler(msg, crct_name)
+    except Exception:
+        print("something went wrong")
+        bot.send_message(1915029649, "kuch glt ho gaya :(")
+
+def crct_name(message):
+    chat_id = message.chat.id
+    mv_name = message.text
+    user = user_dict[chat_id]
+
+    try:
+        bot.send_message(user.u_id.strip(),
+                         f"The movie has been added to the database 😊\nYou can retry now\nTry saying```waste {mv_name.strip()}```",
+                         parse_mode="Markdownv2")
+        bot.send_message(1915029649, "message sent successfully ")
+    except Exception:
+        print("something went wrong")
+        bot.send_message(1915029649, "kuch glt ho gaya :(")
+
+bot.enable_save_next_step_handlers(delay=2)
+
+bot.load_next_step_handlers()
 
 bot.polling()
